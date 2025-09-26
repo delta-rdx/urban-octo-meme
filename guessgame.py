b7f9700b4ad3cf6c58b4f7c4cb257826fe058261
secure-s <talk2shahab123@gmail.com>
2025-09-26 16:33:48 +0500

    more numbers

--- guessgame.py
+++ guessgame.py
@@ -1,12 +1,12 @@
 import random
 
 def number_guessing_game():
-    number_to_guess = random.randint(1, 100)
+    number_to_guess = random.randint(1, 1000)
     guess = None
     attempts = 0
 
     print("Welcome to the Number Guessing Game!")
-    print("I'm thinking of a number between 1 and 100.")
+    print("I'm thinking of a number between 1 and 1000.")
 
     while guess != number_to_guess:
         try:
